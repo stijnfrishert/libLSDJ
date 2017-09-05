395689a4efcfc040b4aa57a82f7e3bf9b46f16b2
Stijn Frishert <info@stijnfrishert.com>
2017-09-05 17:26:14 +0200

    Exporting lsdsng's in the CLI

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
  #include "sav.h"
 
@@ -7,6 +8,23 @@ int main(int argc, char* argv[])
 	for (int i = 1; i < argc; ++i)
 	{
 		lsdj_sav_t* sav = lsdj_open_sav(argv[i], NULL);
+        
+        for (int i = 0; i < sav->project_count; ++i)
+        {
+            char v[3];
+            sprintf(v, "%02X", sav->projects[i].version);
+            v[2] = 0;
+            
+            char path[32];
+            memset(path, 0, 32);
+            strncat(path, sav->projects[i].name, 8);
+
+            strcat(path, ".");
+            strcat(path, v);
+            strcat(path, ".lsdsng");
+            
+            lsdj_write_lsdsng(&sav->projects[i], path, NULL);
+        }
 
 		if (sav)
 			lsdj_close_sav(sav);
