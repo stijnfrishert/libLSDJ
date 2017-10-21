becc2596e0736b764c437475fcd2c82826ae1242
Stijn Frishert <info@stijnfrishert.com>
2017-10-22 00:00:26 +0200

    structures for four instrument type params

--- instrument.h
+++ instrument.h
@@ -18,6 +18,53 @@ static const unsigned char INSTR_TYPE_PULSE = 0;
 static const unsigned char INSTR_TYPE_WAVE = 1;
 static const unsigned char INSTR_TYPE_KIT = 2;
 static const unsigned char INSTR_TYPE_NOISE = 3;
+
+typedef struct
+{
+    unsigned char pulseWidth;
+    unsigned char length;
+    unsigned char sweep;
+    unsigned char plvib;
+    unsigned char vibratoDirection;
+    unsigned char transpose;
+    unsigned char pulse2tune;
+    unsigned char fineTune;
+} lsdj_instrument_pulse_t;
+
+typedef struct
+{
+    unsigned char plvib;
+    unsigned char vibratoDirection;
+    unsigned char transpose;
+    unsigned char synth;
+    unsigned char playback;
+    unsigned char length;
+    unsigned char repeat;
+    unsigned char speed;
+} lsdj_instrument_wave_t;
+
+typedef struct
+{
+    unsigned char sample1;
+    unsigned char sample2;
+    unsigned char pitch;
+    unsigned char offset1;
+    unsigned char length1;
+    unsigned char loop1;
+    unsigned char offset2;
+    unsigned char length2;
+    unsigned char loop2;
+    unsigned char speed;
+    unsigned char distortion;
+    unsigned char pSpeed;
+} lsdj_instrument_kit_t;
+
+typedef struct
+{
+    unsigned char length;
+    unsigned char shape;
+    unsigned char sCommand;
+} lsdj_instrument_noise_t;
     
 // Structure representing one instrument
 typedef struct
@@ -33,28 +80,16 @@ typedef struct
             unsigned char type;
             
             union { unsigned char envelope; unsigned char volume; };
+            unsigned char panning;
+            unsigned char table;
+            unsigned char automate;
             
             union
             {
-                struct
-                {
-                    
-                } pulse;
-                
-                struct
-                {
-                    
-                } wave;
-                
-                struct
-                {
-                    
-                } kit;
-                
-                struct
-                {
-                    
-                } noise;
+                lsdj_instrument_pulse_t pulse;
+                lsdj_instrument_wave_t wave;
+                lsdj_instrument_kit_t kit;
+                lsdj_instrument_noise_t noise;
             };
         };
     };
