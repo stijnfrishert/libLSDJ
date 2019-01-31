140bf383e1e941ff9bc250cc676f5dee638abef0
Stijn Frishert <info@stijnfrishert.com>
2019-01-31 20:46:11 +0100

    Added 2019 to copyright notice

--- liblsdj/wave.h
+++ liblsdj/wave.h
@@ -11,7 +11,7 @@
  
  MIT License
  
- Copyright (c) 2018 Stijn Frishert
+ Copyright (c) 2018 - 2019 Stijn Frishert
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
