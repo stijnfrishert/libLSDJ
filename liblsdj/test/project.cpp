360ae9805deaa31228b8542de235588668d679ee
Stijn Frishert <stijnfrishert@gmail.com>
2020-02-09 13:10:31 +0100

    Added (comment/o) a song write/read test

--- liblsdj/test/project.cpp
+++ liblsdj/test/project.cpp
@@ -4,7 +4,6 @@
 #include <array>
 #include <cassert>
 #include <catch2/catch.hpp>
-#include <fstream>
 
 #include <compression.h>
 #include "file.hpp"
--- liblsdj/test/sav.cpp
+++ liblsdj/test/sav.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <array>
+#include <cassert>
 #include <catch2/catch.hpp>
 
 #include "file.hpp"
