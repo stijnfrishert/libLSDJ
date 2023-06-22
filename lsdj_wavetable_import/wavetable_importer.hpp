4f7eedeb7ce2061d3ed78427be54fcbdd1966132
Stijn Frishert <stijnfrishert@gmail.com>
2023-06-22 15:41:40 +0200

    Added some missing headers

--- lsdj_wavetable_import/wavetable_importer.hpp
+++ lsdj_wavetable_import/wavetable_importer.hpp
@@ -1,28 +1,28 @@
 /*
- 
+
  This file is a part of liblsdj, a C library for managing everything
  that has to do with LSDJ, software for writing music (chiptune) with
  your gameboy. For more information, see:
- 
+
  * https://github.com/stijnfrishert/liblsdj
  * http://www.littlesounddj.com
- 
+
  --------------------------------------------------------------------------------
- 
+
  MIT License
- 
+
  Copyright (c) 2018 - 2020 Stijn Frishert
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in all
  copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -30,7 +30,7 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE.
- 
+
  */
 
 #ifndef LSDJ_WAVETABLE_IMPORTER_HPP
@@ -38,31 +38,33 @@
 
 #include <filesystem>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include <lsdj/error.h>
 #include <lsdj/song.h>
 
-namespace lsdj
-{
-    class WavetableImporter
-    {
-    public:
-        bool import(const std::string& projectName, const std::string& wavetableName);
-        
-    public:
-        std::string outputName;
-        uint8_t wavetableIndex = 0;
-        
-        bool zero = false;
-        bool force = false;
-        bool verbose = false;
-        
-    private:
-        bool importToSav(const std::filesystem::path& path, const std::string& wavetableName);
-        bool importToLsdsng(const std::filesystem::path& path, const std::string& wavetableName);
-        std::pair<bool, unsigned int> importToSong(lsdj_song_t* song, const std::string& wavetableName);
-    };
-}
+namespace lsdj {
+class WavetableImporter {
+public:
+  bool import(const std::string &projectName, const std::string &wavetableName);
+
+public:
+  std::string outputName;
+  uint8_t wavetableIndex = 0;
+
+  bool zero = false;
+  bool force = false;
+  bool verbose = false;
+
+private:
+  bool importToSav(const std::filesystem::path &path,
+                   const std::string &wavetableName);
+  bool importToLsdsng(const std::filesystem::path &path,
+                      const std::string &wavetableName);
+  std::pair<bool, unsigned int> importToSong(lsdj_song_t *song,
+                                             const std::string &wavetableName);
+};
+} // namespace lsdj
 
 #endif
